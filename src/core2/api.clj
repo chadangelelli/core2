ea22bf65f767c5ac992e34b657bbfae0dfca20b3
Chad Angelelli <chad.angelelli@gmail.com>
2021-11-01 21:51:15 -0600

    api: adds grant-user perms

--- src/core2/api.clj
+++ src/core2/api.clj
@@ -16,18 +16,74 @@
    [core2.validation.builtins :as vb]
    [core2.error :refer (err)]))
 
+(def permissions #{:read :update :state :owner :all})
+
+;;TODO: validate input
+(defn make-perms-key
+  [typ perm]
+  (keyword
+   (format "core2/%s-%s"
+           (case typ :user "upm" :group "gpm")
+           (str perm))))
+
+(defn make-user-id-lookup
+  [users]
+  (into {} (for [{:keys [:user/email :xt/id]} (seq users)] [email id])))
+
+(defn make-user-email-lookup
+  [users]
+  (into {} (for [{:keys [:user/email :xt/id]} (seq users)] [id email])))
+
+(defn make-grant-user-perms
+  [id-lookup email-lookup perms grant-user]
+  (if-not perms
+    {}
+    (reduce
+     (fn [o* [u* p*]]
+       (let [user-id (if (v/valid-email? u*) (get id-lookup u*) u*)]
+         (merge
+          o*
+          (reduce (fn [o** p**]
+                    (let [k (make-perms-key :user p**)
+                          v (conj (or (get o* k) #{}) user-id)]
+                      (assoc o** k v)))
+                  o*
+                  p*))))
+     perms
+     grant-user)))
+
+;;TODO: imlement set-owner on create!
+(defn make-new-document-permissions
+  ""
+  {:added "0.1"}
+  [{:keys [user grant-user grant-group set-owner] :as args}]
+
+  (let [user-list (-> (filter identity
+                              (concat (keys grant-user)
+                                      (keys grant-group)
+                                      [set-owner]))
+                      (user/get-users [:xt/id :user/email]))
+        id-lu     (make-user-id-lookup user-list)
+        email-lu  (make-user-email-lookup user-list)
+        perms     {:core2/upm-owner user}
+        perms     (make-grant-user-perms id-lu email-lu perms grant-user)
+
+        ]
+    {:___ID_LU___ id-lu
+     :___EMAIL_LU___ email-lu
+     :___PERMS___ perms}
+    ))
+
 ;;TODO: finish grant, set-state
 (defn make-new-document
   ""
   {:added "0.1"}
-  [{:keys [xt/id user schema data grant set-state] :as args}]
+  [{:keys [xt/id schema data] :as args}]
 
   (let [id (or id (util/uuid))]
     (merge data
-           {:xt/id id
-            :core2/schema schema
-            :core2/owner user
-            :core2/perms-all #{user}})))
+           {:xt/id id, :core2/schema schema}
+           (make-new-document-permissions args))))
 
 (def valid-create-args
   (m/schema
@@ -40,7 +96,7 @@
     [:set-state   {:optional true} vb/valid-state-set]]))
 
 (defn invalidate-create-args
-  [{:keys [user schema data grant set-state] :as args}]
+  [{:keys [user schema data] :as args}]
 
   (if-let [args-err (validate valid-create-args args)]
     (err {:error/type :core2/args-error
@@ -83,22 +139,36 @@
           nil)))))
 
 (defn create!
-  "Takes an argument map to create a document and
-  returns standard core2 `[res err]` vector."
+  "Creates a document. Returns Core2 Data Event."
   {:added "0.1"}
   [{:keys [user] :as args}]
 
   (let [event   (db/make-data-event)
-        user-id (user/get-user user [:xt/id])
+        user-id (:xt/id (user/get-user user [:xt/id]))
         args*   (assoc args :user user-id)]
 
     (if-let [err (invalidate-create-args args*)]
-      [(db/make-data-event-response event)
-       err]
+      (db/make-data-event-response event err)
 
       (let [doc (make-new-document args*)
-            res (db/put {:event event :doc doc})]
-        [res nil]))))
+            ;; res (db/put {:event event :doc doc})
+            res doc
+            ]
+        res))))
+
+  (println "\n\n\n")
+  (time
+   (clojure.pprint/pprint
+    (let [args {:user "chad@shorttrack.io"
+                :schema :User
+                :data {:user/first_name "Chris"
+                       :user/last_name "Hacker"
+                       :user/email "chris@shorttrack.io"}
+                :grant-user {"chad@shorttrack.io" #{:read :update :state}}
+                :set-state #{:hidden}}]
+
+      ;; (make-new-document args)
+      (create! args))))
 
 (defn update!
   ""
@@ -151,8 +221,8 @@
                         #(conj
                           %1
                           (list 'or
-                                [(symbol (name %2)) :core2/perms-all user-id]
-                                [(symbol (name %2)) :core2/perms-read user-id]))
+                                [(symbol (name %2)) :core2/upm-all user-id]
+                                [(symbol (name %2)) :core2/upm-read user-id]))
                         where
                         logic-vars)
                 q* (assoc q :where where*)]
